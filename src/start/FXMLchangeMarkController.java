624d7ae35ef4dc67449893798e5d7ba90b114da4
dorota <dorota@dorota.com>
2018-11-27 21:59:32 +0100

    Zmiana ocen

---
+++ src/start/FXMLchangeMarkController.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package start;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Dorota
+ */
+public class FXMLchangeMarkController implements Initializable {
+
+    @FXML
+    private Button prevBtn;
+    @FXML
+    private Button Logout;
+    @FXML
+    private TextField mark;
+    @FXML
+    private Button btnChange;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void prevWindow(ActionEvent event) throws IOException {
+           Stage dialogStage = (Stage)prevBtn.getScene() .getWindow();
+        dialogStage.close();
+        
+        Stage stage = new Stage();
+                Parent root = FXMLLoader.load(getClass().getResource("FXMLeditMark.fxml"));
+                
+                Scene scene;
+        scene = new Scene(root);
+        
+                stage.setScene(scene);
+                stage.show();
+    }
+
+    @FXML
+    private void logout(ActionEvent event) throws IOException {
+         Stage dialogStage = (Stage)prevBtn.getScene() .getWindow();
+        dialogStage.close();
+        
+        Stage stage = new Stage();
+                Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+                
+                Scene scene;
+        scene = new Scene(root);
+        
+                stage.setScene(scene);
+                stage.show();
+    }
+
+    @FXML
+    private void changeToMark(ActionEvent event) {
+    }
+    
+}
