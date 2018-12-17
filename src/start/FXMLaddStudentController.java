daf87d70a872d538d02f3eec1f7fcb6f0f085b65
Szczena40 <szczech40@gmail.com>
2018-12-17 23:09:38 +0100

    IEGR2-43
    Panel dodawania studenta, przy próbie pojawia się błąd kodu SQL

--- src/start/FXMLaddStudentController.java
+++ src/start/FXMLaddStudentController.java
@@ -2,6 +2,10 @@ package start;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -10,6 +14,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 
@@ -21,6 +26,28 @@ public class FXMLaddStudentController implements Initializable {
     private Button prevBtn;
     @FXML
     private Button confirm;
+    @FXML
+    private TextField pesel;
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private TextField indexNumber;
+    @FXML
+    private TextField degreeCourse;
+    @FXML
+    private TextField password;
+    @FXML
+    private TextField street;
+    @FXML
+    private TextField houseNumber;
+    @FXML
+    private TextField postCode;
+    @FXML
+    private TextField city;
+    @FXML
+    private TextField phoneNumber;
     
     @Override
     
@@ -58,4 +85,38 @@ public class FXMLaddStudentController implements Initializable {
                 stage.show();
     }
     
-}
+    @FXML
+    private void addStudentOnClick(ActionEvent event)throws IOException, SQLException {
+
+        try{
+            PreparedStatement st;
+            Connection myConn=ConnectionManager.getConnection();
+            st = myConn.prepareStatement("INSERT INTO student ( nr_indeksu, imie, nazwisko, haslo, pesel, kierunek_s, ulica, nr_domu, kod_p, miejscowosc, nr_tel VALUES(?,?,?,?,?,?,?,?,?,?,?");
+          
+                st.setString(1, indexNumber.getText());
+                st.setString(2, name.getText());
+                st.setString(3, lastName.getText());
+                st.setString(4, password.getText());
+                st.setString(5, pesel.getText());
+                st.setString(6, degreeCourse.getText());
+                st.setString(7, street.getText());
+                st.setString(8, houseNumber.getText());
+                st.setString(9, postCode.getText());
+                st.setString(10, city.getText());
+                st.setString(11, phoneNumber.getText());
+                st.execute();
+                st.close();
+        }
+        catch(Exception e1){
+            System.err.println(e1);
+             
+        }
+            
+                   
+                    
+        }
+        
+    }
+            
+            
+            
