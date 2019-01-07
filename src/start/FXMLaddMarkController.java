9e7fbeb8cf8d280eff165a024653e4d78cefe622
dorota1396 <38761291+dorota1396@users.noreply.github.com>
2019-01-07 22:06:27 +0100

    Dodanie nowej oceny

--- src/start/FXMLaddMarkController.java
+++ src/start/FXMLaddMarkController.java
@@ -73,7 +73,64 @@ public class FXMLaddMarkController implements Initializable {
     }
 
     @FXML
-    private void addToMark(ActionEvent event) {
+   private void addToMark(ActionEvent event) throws IOException, SQLException {
+        String sql = "INSERT INTO oceny ( ocena, nr_indeksu, nazwa, imie_w, "
+                + "nazwisko_w, id_wykladowcy, id_oceny) VALUES (?,?,?,?,?,?,?)";
+        if (validateFields()) {
+            try {
+
+                try (Connection myConn = ConnectionManager.getConnection()) {
+                    PreparedStatement st = myConn.prepareStatement(sql);
+
+                    st.setString(1, this.newMark.getText());
+                    st.setString(2, this.index.getText());
+                    st.setString(3, this.subject.getText());
+                    st.setString(4, this.name.getText());
+                    st.setString(5, this.surname.getText());
+                    st.setString(6, this.id_w.getText());
+                    st.setString(7, this.id.getText());
+
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Dodanie oceny");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Udało się poprawnie dodać ocenę");
+                    alert.showAndWait();
+
+                    st.execute();
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
-    
+
+    private void clearFields(ActionEvent event) {
+        this.newMark.setText(" ");
+        this.index.setText(" ");
+        this.subject.setText(" ");
+        this.name.setText(" ");
+        this.surname.setText(" ");
+        this.id_w.setText(" ");
+
+    }
+
+    private boolean validateFields() {
+        if (newMark.getText().isEmpty() | index.getText().isEmpty()
+                | subject.getText().isEmpty() | name.getText().isEmpty()
+                | surname.getText().isEmpty() | id_w.getText().isEmpty()) {
+
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Uwaga");
+            alert.setHeaderText(null);
+            alert.setContentText("Proszę wypełnić wszystkie pola");
+            alert.showAndWait();
+
+            return false;
+        }
+        return true;
+    }
 }
+
+
+
