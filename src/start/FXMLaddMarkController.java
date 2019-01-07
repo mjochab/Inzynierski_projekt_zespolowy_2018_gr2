fce65dfce1e7d05953d354d1654076850f934bb5
dorota1396 <38761291+dorota1396@users.noreply.github.com>
2019-01-07 22:19:05 +0100

    Update FXMLaddMarkController.java
    poprawione

--- src/start/FXMLaddMarkController.java
+++ src/start/FXMLaddMarkController.java
@@ -7,6 +7,9 @@ package start;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,6 +17,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -33,6 +37,18 @@ public class FXMLaddMarkController implements Initializable {
     private Button addBtn;
     @FXML
     private TextField newMark;
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField subject;
+    @FXML
+    private TextField surname;
+    @FXML
+    private TextField id_w;
+    @FXML
+    private TextField index;
+    @FXML
+    private TextField id;
 
     /**
      * Initializes the controller class.
